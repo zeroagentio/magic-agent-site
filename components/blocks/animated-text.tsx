e4e0fa0daff05ae0d11c9f38b5651eddfb4326c4
ndrohith09 <ndrohith09@gmail.com>
2024-12-20 15:07:22 +0530

    fix

--- components/blocks/animated-text.tsx
+++ components/blocks/animated-text.tsx
@@ -1,15 +1,21 @@
 import { ChevronRight } from "lucide-react";
 
-import { cn } from "@/lib/utils"; 
+import { cn, WAITINGLIST_FORM } from "@/lib/utils"; 
 import AnimatedShinyText from "../magicui/animated-shiny-text";
 
 export async function AnimatedGradientTextDemo() {
+
+    const handleClick = () => {
+    window.open(WAITINGLIST_FORM, '_blank'); // Open in a new tab
+  };
+
   return (
     <div className="z-10 flex items-center justify-center">
       <div
         className={cn(
           "group rounded-full border border-black/5 bg-gray-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800",
         )}
+        onClick={handleClick}
       >
       <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out text-black hover:duration-300">
           <span>✨ Future of Online Shopping is here</span>  
