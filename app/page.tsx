03a4d6767f4d507a2d8ce0ef18dfc087c55ebb2a
ndrohith09 <ndrohith09@gmail.com>
2025-03-12 20:35:58 +0530

    fix: mixpanel

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,45 @@
-// 'use client';
+"use client";
 import { BannerCard } from "@/components/blocks/banner-card";
 import Chat from "@/components/blocks/chat";
-import CTA from "@/components/blocks/cta"; 
-import FeatureGrid from "@/components/blocks/feature-grid"; 
+import CTA from "@/components/blocks/cta";
+import FeatureGrid from "@/components/blocks/feature-grid";
 import Footer from "@/components/blocks/footer";
-import Header from "@/components/blocks/header";    ;
-import Navbar from "@/components/blocks/navbar"; 
+import Header from "@/components/blocks/header";
+import Navbar from "@/components/blocks/navbar";
 import TalkWithZero from "@/components/blocks/talk-with-zero";
-import UseCase from "@/components/blocks/use-case"; 
+import UseCase from "@/components/blocks/use-case";
 import mixpanel from "mixpanel-browser";
+import { useEffect } from "react";
 
 const MIXPANEL_ENABLED = process.env.NODE_ENV === "production";
 
-
 export default function Home() {
+  useEffect(() => {
+    if (MIXPANEL_ENABLED) {
+      console.log("mixpanel initi");
 
-
-  if (MIXPANEL_ENABLED) {
-
-    mixpanel.init('87b1ee5af8f55762dea98975d8ba744f', {
-      debug: false,
-      track_pageview: true,
-      persistence: "localStorage",
-    }); 
-    console.log("mixpanel", mixpanel);
-  }
+      mixpanel.init("76c4103b3db403a23d99266da36795ab", {
+        debug: false,
+        track_pageview: true,
+        persistence: "localStorage",
+      });
+    }
+  }, []);
 
   return (
-    <> 
-    <Navbar />
-    <div  
-    className="relative mx-auto my-28 max-w-[80rem] px-6 text-center md:px-8"
-    >  
-      <Header />
-      <BannerCard />
-      <Chat /> 
-      <TalkWithZero /> 
-      <FeatureGrid />
-      <UseCase />
-      </div> 
+    <>
+      <Navbar />
+      <div className="relative mx-auto my-28 max-w-[80rem] px-6 text-center md:px-8">
+        <Header />
+        <BannerCard />
+        <Chat />
+        <TalkWithZero />
+        <FeatureGrid />
+        <UseCase />
+      </div>
       <CTA />
-      <br /> 
-      <Footer /> 
+      <br />
+      <Footer />
     </>
   );
 }
