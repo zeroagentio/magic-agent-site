54269777af8dfe40f46dca84de816833f07232e1
ndrohith09 <ndrohith09@gmail.com>
2025-04-11 21:47:27 +0530

    change header tagline

--- components/blocks/header.tsx
+++ components/blocks/header.tsx
@@ -4,6 +4,10 @@ import Particles from "../magicui/particles";
 import { AnimatedGradientTextDemo } from "./animated-text";
 import { VIEW_DEMO, MY_APP } from "@/lib/utils";
 import { InteractiveHoverButton } from "../magicui/interactive-hover-button";
+import ZeroPersona from "@/app/assets/zero-persona.png";
+import Image from "next/image";
+
+
 export function GetFree() {
 
   const handleClick = () => {
@@ -30,28 +34,47 @@ export function Demo() {
 const Header = () => {
   return (
     <>
+                <div className="mx-auto max-w-2xl px-4 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-5  lg:gap-x-8 lg:px-8 lg:pt-16">
+    
+                <div className="lg:col-span-3">
      <Particles
         className="absolute inset-0"
-        quantity={100}
+        quantity={1000}
         ease={100}
         color={'#ffffff'} 
         refresh
       />
       <AnimatedGradientTextDemo />  
       <h1 className="mt-7 mb-3 bg-gradient-to-br  bg-clip-text py-6 text-5xl  font-semibold leading-none tracking-tight text-black text-balance sm:text-6xl md:text-7xl lg:text-7xl translate-y-[-1rem] animate-fade-in opacity-1 [--animation-delay:200ms]">              
-        Meet Zero, Your AI Shopping
-        <br className="hidden md:block" />&nbsp;Assistant who Speaks & Sells 
+        Hire Zero, Your AI Store Employee who Speaks, Sells & More 
+        {/* <br className="hidden md:block" />&nbsp; Shopping Assistant */}
       </h1>
 
       <div className="max-w-5xl mx-auto text-center">
       <p className="mb-7 text-lg leading-loose font-medium tracking-normal text-gray-800 md:text-2xl text-balance translate-y-[-1rem] animate-fade-in opacity-1 [--animation-delay:400ms]">
-      Talk to AI that offers personalized product recommendations, handles orders, answers query & more for customers.<br />
-      <span className="font-semibold">"Just like shopping in-store, but online"</span>
+      {/* AI Employee that offers personalized product recommendations, handles customer support, analyzes store performance & much more.<br /> */}
+      The 24/7 AI Employee that boosts sales, handles customer support, analyzes store performance & much more. <br />
+      <span className="font-semibold">"Focus on business while AI Handles the rest"</span>
+      {/* <span className="font-semibold">"Just like shopping in-store, but online"</span> */}
       </p>
       </div>
       <div className="space-x-2">
       <GetFree />    
       <Demo />    
+      </div>
+      </div>
+
+        <div className="lg:col-start-4 lg:col-span-2"> 
+                          
+                          <Image 
+                          alt=''
+                          className='lg:max-w-[25rem] max-w-[20rem] w-auto h-full'
+                         //  width={400}
+                         //  height={700}
+                          src={ZeroPersona}
+                          />
+                       </div>
+
       </div>
       </>
   );
